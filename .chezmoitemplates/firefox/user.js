7a9eb1686777c16d6d16e197f6d53744744e410e
Quinton Cloutier <quintoncloutier@outlook.com>
2023-07-22 11:45:34 -0300

    Add firefox

---
+++ .chezmoitemplates/firefox/user.js
@@ -0,0 +1,60 @@
+user_pref("app.shield.optoutstudies.enabled", false);
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.contentblocking.category", "strict");
+user_pref("browser.download.useDownloadDir", false);
+user_pref("browser.link.open_newwindow.restriction", 0); // enforce
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
+user_pref("browser.newtabpage.activity-stream.default.sites", "");
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts", false);
+user_pref("browser.newtabpage.activity-stream.showSponsored", false);
+user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
+user_pref("browser.newtabpage.blocked", '{"26UbzFJ7qT9/4DhodHKA1Q==":1,"4gPpjkxgZzXPVtuEoAL9Ig==":1,"gLv0ja2RYVgxKdp0I5qwvA==":1,"eV8/WsSLxHadrTL1gAxhug==":1,"oYry01JR5qiqP3ru9Hdmtg==":1,"T9nJot5PurhJSy8n038xGA==":1}'); // hide defaults
+user_pref("browser.search.hiddenOneOffs", "Google,Amazon.ca,Bing,DuckDuckGo,eBay,Wikipedia (en)");
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("browser.startup.couldRestoreSession.count", 2); // dismiss
+user_pref("browser.startup.homepage_override.mstone", "ignore");
+user_pref("browser.startup.page", 3); // open previous windows and tabs
+user_pref("browser.tabs.closeWindowWithLastTab", false);
+user_pref("browser.tabs.drawInTitlebar", true);
+user_pref("browser.toolbars.bookmarks.visibility", "never");
+user_pref("browser.uiCustomization.state", '{"placements":{"nav-bar":["back-button","forward-button","stop-reload-button","customizableui-special-spring1","urlbar-container","customizableui-special-spring2","downloads-button","unified-extensions-button"],"TabsToolbar":["alltabs-button","tabbrowser-tabs","new-tab-button"]},"currentVersion":10000}');
+user_pref("browser.uidensity", 1); // compact
+user_pref("browser.urlbar.shortcuts.bookmarks", false);
+user_pref("browser.urlbar.shortcuts.history", false);
+user_pref("browser.urlbar.shortcuts.tabs", false);
+user_pref("browser.urlbar.tipShownCount.searchTip_onboard", 4); // dismiss
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
+user_pref("devtools.selfxss.count", 5); // allow pasting
+user_pref("devtools.toolbox.host", "right");
+user_pref("dom.popup_allowed_events", "click dblclick mousedown pointerdown");
+user_pref("dom.security.https_only_mode", true);
+user_pref("extensions.activeThemeID", "firefox-compact-dark@mozilla.org");
+user_pref("extensions.autoDisableScopes", 0); // no restrictions
+user_pref("extensions.formautofill.addresses.enabled", false);
+user_pref("extensions.formautofill.creditCards.enabled", false);
+user_pref("extensions.formautofill.heuristics.enabled", false);
+user_pref("extensions.getAddons.showPane", false);
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
+user_pref("extensions.pocket.enabled", false);
+user_pref("extensions.webextensions.restrictedDomains", "");
+user_pref("full-screen-api.warning.timeout", 0); // never show
+user_pref("general.autoScroll", true);
+user_pref("geo.provider.network.url", "https://location.services.mozilla.com/v1/geolocate?key=%MOZILLA_API_KEY%");
+user_pref("media.autoplay.default", 5); // block audio and video
+user_pref("media.hardware-video-decoding.force-enabled", true);
+user_pref("network.IDN_show_punycode", true);
+user_pref("network.stricttransportsecurity.preloadlist", false); // allow exceptions
+user_pref("privacy.clearOnShutdown.history", false);
+user_pref("privacy.clearOnShutdown.offlineApps", true);
+user_pref("privacy.sanitize.sanitizeOnShutdown", true);
+user_pref("privacy.sanitize.timeSpan", 0); // everything
+user_pref("privacy.userContext.enabled", true); // container tabs
+user_pref("privacy.userContext.ui.enabled", true);
+user_pref("services.sync.declinedEngines", "passwords,addresses,creditcards,prefs,addons");
+user_pref("signon.rememberSignons", false);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("toolkit.telemetry.unified", false);
---
+++ dot_var/app/org.mozilla.firefox/dot_mozilla/firefox/00000000.default-release/extensions/run_onchange_install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+wget https://addons.mozilla.org/firefox/downloads/file/4121906/ublock_origin-latest.xpi \
+  -O 'uBlock0@raymondhill.net.xpi'
+
+wget https://addons.mozilla.org/firefox/downloads/file/4095037/darkreader-latest.xpi \
+  -O 'addon@darkreader.org.xpi'
+
+wget https://addons.mozilla.org/firefox/downloads/file/4094964/keepassxc_browser-latest.xpi \
+  -O 'keepassxc-browser@keepassxc.org.xpi'
