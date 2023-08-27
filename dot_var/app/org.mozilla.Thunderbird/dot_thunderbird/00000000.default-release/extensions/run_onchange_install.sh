f1db1848bd38845ed4950710325c5749508f47a2
Quinton Cloutier <quintoncloutier@outlook.com>
2023-08-27 17:52:02 -0300

    Add thunderbird

---
+++ dot_var/app/org.mozilla.Thunderbird/dot_thunderbird/00000000.default-release/extensions/run_onchange_install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+wget https://addons.thunderbird.net/thunderbird/downloads/latest/ublock-origin/addon-988489-latest.xpi \
+  -O 'uBlock0@raymondhill.net.xpi'
+
+wget https://addons.thunderbird.net/thunderbird/downloads/latest/darkreader/addon-988018-latest.xpi \
+  -O 'addon@darkreader.org.xpi'
---
+++ dot_var/app/org.mozilla.Thunderbird/dot_thunderbird/00000000.default-release/user.js
@@ -0,0 +1,26 @@
+user_pref("app.donation.eoy.version.viewed", 2); // dismiss
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
+user_pref("dom.popup_allowed_events", "click dblclick mousedown pointerdown");
+user_pref("dom.security.https_only_mode", true);
+user_pref("extensions.activeThemeID", "thunderbird-compact-dark@mozilla.org");
+user_pref("extensions.autoDisableScopes", 0); // no restrictions
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
+user_pref("extensions.webextensions.restrictedDomains", "");
+user_pref("general.autoScroll", true);
+user_pref("geo.provider.network.url", "https://location.services.mozilla.com/v1/geolocate?key=%MOZILLA_API_KEY%");
+user_pref("mail.pane_config.dynamic", 2); // vertical
+user_pref("mail.rights.version", 1); // dismiss
+user_pref("mail.spam.manualMark", true);
+user_pref("mailnews.default_sort_order", 2); // descending
+user_pref("mailnews.default_sort_type", 18); // by date
+user_pref("mailnews.start_page_override.mstone", "ignore");
+user_pref("media.hardware-video-decoding.force-enabled", true);
+user_pref("network.IDN_show_punycode", true);
+user_pref("network.cookie.cookieBehavior", 2); // do not accept
+user_pref("places.history.enabled", false);
+user_pref("privacy.clearOnShutdown.cache", true);
+user_pref("privacy.sanitize.sanitizeOnShutdown", true);
+user_pref("rss.show.summary", 0); // web page
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("toolkit.telemetry.unified", false);
