725bbcb838776ea62e1e24848f9279943afa14c7
Quinton Cloutier <quintoncloutier@outlook.com>
2024-05-02 22:29:01 -0300

    Update fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -7,30 +7,35 @@ set -gx AWS_CONFIG_FILE $XDG_CONFIG_HOME/aws/config
 set -gx CLICKHOUSE_HISTORY_FILE $XDG_STATE_HOME/clickhouse-client-history
 set -gx GOPATH $XDG_DATA_HOME/go
 set -gx HISTFILE $XDG_STATE_HOME/bash_history
-set -gx KDEHOME $XDG_CONFIG_HOME/kde
+set -gx KREW_ROOT $XDG_DATA_HOME/krew
 set -gx LESSHISTFILE $XDG_STATE_HOME/lesshst
 set -gx NPM_CONFIG_USERCONFIG $XDG_CONFIG_HOME/npm/npmrc
 set -gx PSQL_HISTORY $XDG_STATE_HOME/psql_history
+set -gx TENV_ROOT $XDG_DATA_HOME/tenv
 set -gx WGETRC $XDG_CONFIG_HOME/wget/wgetrc
 
-set -gx BROWSER firefox
+set -gx BROWSER org.mozilla.firefox
 set -gx EDITOR nvim
 set -gx TERMINAL kitty
 
 set -gx AWS_EC2_METADATA_DISABLED true
+set -gx AWS_VAULT_SECRET_SERVICE_COLLECTION_NAME login
 set -gx BAT_OPTS --plain --theme=base16-256
-set -gx DOCKER_BUILDKIT 1
 set -gx DOCKER_HOST unix://$XDG_RUNTIME_DIR/docker.sock
 set -gx FZF_DEFAULT_OPTS --inline-info --layout=reverse
+
+fish_add_path $KREW_ROOT/bin
+fish_add_path $GOPATH/bin
+fish_add_path $HOME/.local/bin
 
 if status is-interactive
     fish_vi_key_bindings
 
-    set -g fish_greeting
     set -g fish_cursor_default block blink
     set -g fish_cursor_insert line blink
     set -g fish_cursor_replace_one underscore blink
     set -g fish_cursor_visual block blink
+    set -g fish_greeting
 
     abbr -a av aws-vault
     abbr -a cz chezmoi
@@ -41,7 +46,7 @@ if status is-interactive
     abbr -a tf terraform
     abbr -a tg terragrunt
 
-    alias aws-vault 'aws-vault --keychain=login'
+    alias aws-vault 'set -e AWS_VAULT; command aws-vault'
     alias icat 'kitten icat --align=left'
     alias ssh 'kitten ssh'
     alias userctl 'systemctl --user'
